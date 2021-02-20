18b4ba8a92bbdb5e264b867e678f9ced4e3d30dc
huwenchao <me@huwenchao.com>
2021-02-20 19:51:41 +0800

    feat: add basic configs

---
+++ offchain-modules/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['plugin:@typescript-eslint/recommended'],
+  plugins: ['prettier'],
+  ignorePatterns: ['packages/*/lib', '*.js'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+  },
+};
