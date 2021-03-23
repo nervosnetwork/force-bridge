9591cba224a8294994879c65636863d1bd6e2fef
jacobdenver007 <jacobdenver@163.com>
2021-03-23 20:00:38 +0800

    feat(eth-contracts): add multisig for unlock & add changeValidators

--- eth-contracts/test/force-bridge-test.js
+++ eth-contracts/test/force-bridge-test.js
@@ -1,72 +1,175 @@
+const { ethers } = require("hardhat");
 const { expect } = require("chai");
-const { waitingForReceipt } = require("./utils");
+const { keccak256, defaultAbiCoder, toUtf8Bytes } = ethers.utils;
+const {
+  log,
+  waitingForReceipt,
+  generateWallets,
+  getUnlockMsgHash,
+  generateSignatures,
+  getChangeValidatorsMsgHash,
+} = require("./utils");
 
-describe("ForceBridge", function() {
-    it("should work well for lock and unlock ETH", async function() {
-        const abi = require('../artifacts/contracts/ForceBridge.sol/ForceBridge.json').abi;
-        // console.dir(abi, {depth: null});
-        const iface = new ethers.utils.Interface(abi);
+describe("ForceBridge", () => {
+  let forceBridge, adminAddress, contractAddress, provider, factory;
+  let wallets, validators;
+  let multisigThreshold, chainId, DOMAIN_SEPARATOR, unlockTypeHash;
+  let initBlockNumber, latestBlockNumber, historyTxRoot, txRootProofDataVec;
 
-        // deploy
-        const ForceBridge = await ethers.getContractFactory("ForceBridge");
-        const bridge = await ForceBridge.deploy();
-        await bridge.deployed();
-        console.log("ForceBridge deployed to:", bridge.address);
-        const provider = bridge.provider;
+  before(async function () {
+    // disable timeout
+    this.timeout(0);
 
-        // lock
-        const recipientLockscript = '0x00';
-        const sudtExtraData = '0x01';
-        const amount = ethers.utils.parseEther("0.1");
-        const res = await bridge.lockETH(
-            recipientLockscript,
-            sudtExtraData,
-            { value: amount },
-        );
+    const [signer] = await ethers.getSigners();
+    adminAddress = signer.address;
 
-        const receipt = await waitingForReceipt(provider, res);
-        // console.log(`gasUsed: ${receipt.gasUsed.toString()}`);
-        console.dir(receipt, {depth: null});
-        const parsedLog = iface.parseLog(receipt.logs[0]);
-        // console.dir(parsedLog.args, {depth: null});
+    // get validators
+    wallets = generateWallets(7);
+    validators = wallets.map((wallet) => wallet.address);
+    multisigThreshold = 5;
+    chainId = await signer.getChainId();
 
-        expect(parsedLog.args.token).to.equal(
-            '0x0000000000000000000000000000000000000000'
-        );
-        expect(parsedLog.args.lockedAmount).to.equal(amount);
-        expect(parsedLog.args.sudtExtraData).to.equal(sudtExtraData);
-        expect(parsedLog.args.recipientLockscript).to.equal(recipientLockscript);
-        // const balance = await provider.getBalance(bridge.address);
-        // console.log(`contract balance: ${balance}`);
+    // deploy ForceBridge
+    factory = await ethers.getContractFactory(
+      "contracts/ForceBridge.sol:ForceBridge"
+    );
 
-        // unlock
-        const records = [
-            {
-                token: '0x0000000000000000000000000000000000000000',
-                recipient: '0x1000000000000000000000000000000000000001',
-                amount: ethers.utils.parseEther("0.06"),
-                ckbTxHash: '0x1000000000000000000000000000000000000008',
-            },
-            {
-                token: '0x0000000000000000000000000000000000000000',
-                recipient: '0x1000000000000000000000000000000000000002',
-                amount: ethers.utils.parseEther("0.04"),
-                ckbTxHash: '0x1000000000000000000000000000000000000009',
-            },
-        ]
-        const resUnlock = await bridge.unlock(records);
+    forceBridge = await factory.deploy();
+    await forceBridge.deployTransaction.wait(1);
+    const res = await forceBridge.initialize(validators, multisigThreshold);
+    await res.wait(1);
 
-        const receiptUnlock = await waitingForReceipt(provider, resUnlock);
-        // console.dir(receiptUnlock, {depth: null});
-        const unlockLogs = receiptUnlock.logs.map(l => iface.parseLog(l).args);
-        // console.dir(unlockLogs, {depth: null});
-        for(let i=0; i<records.length; i++) {
-            const r = records[i];
-            const res = unlockLogs[i];
-            expect(r.recipient).to.equal(res.recipient);
-            expect(r.token).to.equal(res.token);
-            expect(r.amount).to.equal(res.receivedAmount);
-            expect(r.ckbTxHash).to.equal(res.ckbTxHash);
-        }
+    contractAddress = forceBridge.address;
+    provider = forceBridge.provider;
+  });
+
+  describe("correct case", async function () {
+    // disable timeout
+    this.timeout(0);
+
+    it("check SIGNATURE_SIZE, name, AddHistoryTxRootTypeHash, DOMAIN_SEPARATOR", async () => {
+      expect(await forceBridge.SIGNATURE_SIZE()).to.eq(65);
+
+      const name = "Force Bridge";
+      expect(await forceBridge.NAME_712()).to.eq(name);
+
+      unlockTypeHash = keccak256(
+        toUtf8Bytes("unlock(UnlockRecord[] calldata records)")
+      );
+      console.log(`unlockTypeHash`, unlockTypeHash);
+      expect(await forceBridge.UNLOCK_TYPEHASH()).to.eq(unlockTypeHash);
+
+      DOMAIN_SEPARATOR = keccak256(
+        defaultAbiCoder.encode(
+          ["bytes32", "bytes32", "bytes32", "uint256", "address"],
+          [
+            keccak256(
+              toUtf8Bytes(
+                "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+              )
+            ),
+            keccak256(toUtf8Bytes(name)),
+            keccak256(toUtf8Bytes("1")),
+            chainId,
+            forceBridge.address,
+          ]
+        )
+      );
+      expect(await forceBridge.DOMAIN_SEPARATOR()).to.eq(DOMAIN_SEPARATOR);
     });
+    it("should work well for lock and unlock ETH", async function () {
+      const abi = require("../artifacts/contracts/ForceBridge.sol/ForceBridge.json")
+        .abi;
+      const iface = new ethers.utils.Interface(abi);
+
+      // lock
+      const recipientLockscript = "0x00";
+      const sudtExtraData = "0x01";
+      const amount = ethers.utils.parseEther("0.1");
+      const res = await forceBridge.lockETH(
+        recipientLockscript,
+        sudtExtraData,
+        {
+          value: amount,
+        }
+      );
+
+      const receipt = await waitingForReceipt(provider, res);
+      console.dir(receipt, { depth: null });
+      const parsedLog = iface.parseLog(receipt.logs[0]);
+
+      expect(parsedLog.args.token).to.equal(
+        "0x0000000000000000000000000000000000000000"
+      );
+      expect(parsedLog.args.lockedAmount).to.equal(amount);
+      expect(parsedLog.args.sudtExtraData).to.equal(sudtExtraData);
+      expect(parsedLog.args.recipientLockscript).to.equal(recipientLockscript);
+
+      // unlock
+      const records = [
+        {
+          token: "0x0000000000000000000000000000000000000000",
+          recipient: "0x1000000000000000000000000000000000000001",
+          amount: ethers.utils.parseEther("0.06"),
+          ckbTxHash: "0x1000000000000000000000000000000000000008",
+        },
+        {
+          token: "0x0000000000000000000000000000000000000000",
+          recipient: "0x1000000000000000000000000000000000000002",
+          amount: ethers.utils.parseEther("0.04"),
+          ckbTxHash: "0x1000000000000000000000000000000000000009",
+        },
+      ];
+
+      const msgHash = getUnlockMsgHash(
+        DOMAIN_SEPARATOR,
+        unlockTypeHash,
+        records
+      );
+
+      console.log("msg hash ", msgHash);
+      // 2. generate signatures
+      let signatures = generateSignatures(
+        msgHash,
+        wallets.slice(0, multisigThreshold)
+      );
+
+      const resUnlock = await forceBridge.unlock(records, signatures);
+
+      const receiptUnlock = await waitingForReceipt(provider, resUnlock);
+      // console.dir(receiptUnlock, {depth: null});
+      const unlockLogs = receiptUnlock.logs.map((l) => iface.parseLog(l).args);
+      // console.dir(unlockLogs, {depth: null});
+      for (let i = 0; i < records.length; i++) {
+        const r = records[i];
+        const res = unlockLogs[i];
+        expect(r.recipient).to.equal(res.recipient);
+        expect(r.token).to.equal(res.token);
+        expect(r.amount).to.equal(res.receivedAmount);
+        expect(r.ckbTxHash).to.equal(res.ckbTxHash);
+      }
+    });
+    it("should change validators", async function () {
+      const newWallets = generateWallets(7);
+      newValidators = newWallets.map((wallet) => wallet.address);
+      newMultisigThreshold = 5;
+
+      const msgHash = getChangeValidatorsMsgHash(
+        DOMAIN_SEPARATOR,
+        unlockTypeHash,
+        newValidators,
+        newMultisigThreshold
+      );
+
+      // 2. generate signatures
+      let signatures = generateSignatures(msgHash, wallets.slice(0, 7));
+
+      const res = await forceBridge.changeValidators(
+        newValidators,
+        newMultisigThreshold,
+        signatures
+      );
+      console.log("res", res);
+    });
+  });
 });
--- eth-contracts/test/utils.js
+++ eth-contracts/test/utils.js
@@ -1,31 +1,111 @@
-// const { ecsign, toRpcSig } = require('ethereumjs-util');
-// const { blake2b, PERSONAL } = require('@nervosnetwork/ckb-sdk-utils');
-// const { keccak256, defaultAbiCoder, solidityPack } = ethers.utils;
-// const BN = require('bn.js');
+const { ecsign, toRpcSig } = require("ethereumjs-util");
+const { keccak256, defaultAbiCoder, solidityPack } = ethers.utils;
 
 async function sleep(seconds) {
-    // console.log(`waiting for block confirmations, about ${seconds}s`)
-    await new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+  // console.log(`waiting for block confirmations, about ${seconds}s`)
+  await new Promise((resolve) => setTimeout(resolve, seconds * 1000));
 }
 
 async function waitingForReceipt(provider, res) {
-    if (!res) {
-        return -1;
-    }
+  if (!res) {
+    return -1;
+  }
 
-    const txHash = res.hash;
-    let txReceipt;
-    while (!txReceipt) {
-        txReceipt = await provider.getTransactionReceipt(txHash);
-        if (txReceipt && txReceipt.blockHash) {
-            break;
-        }
-        await sleep(1);
+  const txHash = res.hash;
+  let txReceipt;
+  while (!txReceipt) {
+    txReceipt = await provider.getTransactionReceipt(txHash);
+    if (txReceipt && txReceipt.blockHash) {
+      break;
     }
-    return txReceipt;
+    await sleep(1);
+  }
+  return txReceipt;
 }
+
+const generateWallets = (size) => {
+  const wallets = [];
+  for (let i = 0; i < size; i++) {
+    const wallet = ethers.Wallet.createRandom();
+    wallets.push(wallet);
+  }
+  return wallets;
+};
+
+const generateSignatures = (msgHash, wallets) => {
+  let signatures = "0x";
+  for (let i = 0; i < wallets.length; i++) {
+    const wallet = wallets[i];
+    const { v, r, s } = ecsign(
+      Buffer.from(msgHash.slice(2), "hex"),
+      Buffer.from(wallet.privateKey.slice(2), "hex")
+    );
+    const sigHex = toRpcSig(v, r, s);
+    signatures += sigHex.slice(2);
+  }
+  return signatures;
+};
+
+const getUnlockMsgHash = (DOMAIN_SEPARATOR, typeHash, records) => {
+  return keccak256(
+    solidityPack(
+      ["bytes1", "bytes1", "bytes32", "bytes32"],
+      [
+        "0x19",
+        "0x01",
+        DOMAIN_SEPARATOR,
+        keccak256(
+          defaultAbiCoder.encode(
+            [
+              "bytes32",
+              {
+                components: [
+                  { name: "token", type: "address" },
+                  { name: "recipient", type: "address" },
+                  { name: "amount", type: "uint256" },
+                  { name: "ckbTxHash", type: "bytes" },
+                ],
+                name: "records",
+                type: "tuple[]",
+              },
+            ],
+            [typeHash, records]
+          )
+        ),
+      ]
+    )
+  );
+};
+
+const getChangeValidatorsMsgHash = (
+  DOMAIN_SEPARATOR,
+  typeHash,
+  validators,
+  multisigThreshold
+) => {
+  return keccak256(
+    solidityPack(
+      ["bytes1", "bytes1", "bytes32", "bytes32"],
+      [
+        "0x19",
+        "0x01",
+        DOMAIN_SEPARATOR,
+        keccak256(
+          defaultAbiCoder.encode(
+            ["bytes32", "address[]", "uint256"],
+            [typeHash, validators, multisigThreshold]
+          )
+        ),
+      ]
+    )
+  );
+};
 
 module.exports = {
-    sleep,
-    waitingForReceipt,
-}
+  sleep,
+  waitingForReceipt,
+  generateWallets,
+  generateSignatures,
+  getUnlockMsgHash,
+  getChangeValidatorsMsgHash,
+};
