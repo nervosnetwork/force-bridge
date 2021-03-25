00cc0e7a429bb68372e9ffd216fc49fdcd42c8a0
JacobDenver007 <29848184+JacobDenver007@users.noreply.github.com>
2021-03-25 15:24:48 +0800

    feat(eth-contracts): add multiSign for unlock  (#13)
    * feat(eth-contracts): add multisig for unlock & add changeValidators
    
    * feat(eth-contracts): add ci
    
    * fix(eth-contracts): fix tests
    
    * fix(eth-contracts): fix eth offchain
    
    * mv eth-contracts.yml
    
    * fix package.json
    
    * fix lint
    
    * add solhint and prettier
    
    * add testcases
    
    * add test
    
    * add code coverage script
    
    * rm codecov.yml

--- eth-contracts/hardhat.config.js
+++ eth-contracts/hardhat.config.js
@@ -1,8 +1,9 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
+require('solidity-coverage');
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
+task('accounts', 'Prints the list of accounts', async () => {
   const accounts = await ethers.getSigners();
 
   for (const account of accounts) {
@@ -20,7 +21,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.0",
+        version: '0.8.0',
         settings: {
           optimizer: {
             enabled: true,
@@ -31,7 +32,7 @@ module.exports = {
     ]
   },
 
-  defaultNetwork: "hardhat",
+  defaultNetwork: 'hardhat',
 
   networks: {
     hardhat: {},
@@ -40,18 +41,17 @@ module.exports = {
       // address [`0x17c4b5CE0605F63732bfd175feCe7aC6b4620FD2`, `0x46beaC96B726a51C5703f99eC787ce12793Dae11`]
       // Mnemonic [`dignity vehicle fuel siren cool machine video spice oppose olympic polar discover`, ``]
       accounts: [
-        "c4ad657963930fbff2e9de3404b30a4e21432c89952ed430b56bf802945ed37a",
-        "719e94ec5d2ecef67b5878503ffd6e1e0e2fe7a52ddd55c436878cb4d52d376d",
-        "627ed509aa9ef55858d01453c62f44287f639a4fa5a444af150f333b6010a3b6",
-        "49e7074797d83cbb93b23877f99a8cecd6f79181f1236f095671017b2edc64c2",
-        "6e51216cbb2fe170368da49e82b22f02b999204730c858482d0e84a9083005ac",
-        "ca2e37b4f2e4a122cc86c401e3d1da3841c525f01b5b249dcdcd69e2f086d576"
-      ],
-    },
+        'c4ad657963930fbff2e9de3404b30a4e21432c89952ed430b56bf802945ed37a',
+        '719e94ec5d2ecef67b5878503ffd6e1e0e2fe7a52ddd55c436878cb4d52d376d',
+        '627ed509aa9ef55858d01453c62f44287f639a4fa5a444af150f333b6010a3b6',
+        '49e7074797d83cbb93b23877f99a8cecd6f79181f1236f095671017b2edc64c2',
+        '6e51216cbb2fe170368da49e82b22f02b999204730c858482d0e84a9083005ac',
+        'ca2e37b4f2e4a122cc86c401e3d1da3841c525f01b5b249dcdcd69e2f086d576'
+      ]
+    }
   },
 
   mocha: {
     timeout: 30000
   }
 };
-
