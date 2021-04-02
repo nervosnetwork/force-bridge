8920798fc92cadf39213c44bafdd2b5ea200676a
LeonLi000 <matrix.skygirl@gmail.com>
2021-04-02 16:07:15 +0800

    minor change

--- offchain-modules/src/apps/relayer/index.ts
+++ offchain-modules/src/apps/relayer/index.ts
@@ -41,11 +41,11 @@ async function main() {
     const ethHandler = new EthHandler(ethDb, ethChain);
     ethHandler.start();
   }
-  // if (config.tron !== undefined) {
-  //   const tronDb = new TronDb(conn);
-  //   const tronHandler = new TronHandler(tronDb);
-  //   tronHandler.start();
-  // }
+  if (config.tron !== undefined) {
+    const tronDb = new TronDb(conn);
+    const tronHandler = new TronHandler(tronDb);
+    tronHandler.start();
+  }
   // if (config.btc !== undefined) {
   //   const btcDb = new BtcDb(conn);
   //   const btcChain = new BTCChain();
