0df05f7d3f5471a8371ee61da2169df359da6582
LeonLi000 <matrix.skygirl@gmail.com>
2021-04-07 19:32:25 +0800

    feat: support btc cross-chain

--- offchain-modules/src/packages/ckb/model/asset.ts
+++ offchain-modules/src/packages/ckb/model/asset.ts
@@ -86,3 +86,28 @@ export class EosAsset extends Asset {
     return toHexString(stringToUint8Array(this.address));
   }
 }
+
+export class BtcAsset extends Asset {
+  // '0x00000000000000000000' represents ETH
+  // other address represents ERC20 address
+  constructor(public address: string, public ownLockHash: string) {
+    super();
+    // if (!address.startsWith('') || address.length !== 42) {
+    //   throw new Error('invalid Tron asset address');
+    // }
+    this.chainType = ChainType.BTC;
+  }
+
+  toBridgeLockscriptArgs(): string {
+    const params = {
+      owner_lock_hash: fromHexString(this.ownLockHash).buffer,
+      chain: this.chainType,
+      asset: fromHexString(toHexString(stringToUint8Array(this.address))).buffer,
+    };
+    return `0x${toHexString(new Uint8Array(SerializeForceBridgeLockscriptArgs(params)))}`;
+  }
+
+  getAddress(): string {
+    return toHexString(stringToUint8Array(this.address));
+  }
+}
