64bb22afa8a2c6d07563617f7b47445bc6f1e167
LeonLi000 <matrix.skygirl@gmail.com>
2021-04-20 01:02:34 +0800

    chore: test github ci

--- offchain-modules/src/packages/ckb/tx-helper/multisig/init_config.ts
+++ offchain-modules/src/packages/ckb/tx-helper/multisig/init_config.ts
@@ -3,13 +3,14 @@ import { initializeConfig } from '@ckb-lumos/config-manager';
 
 export function init() {
   const configFilePath = __dirname + '/config.json';
-  if (
-    process.env.LUMOS_CONFIG_NAME !== 'LINA' &&
-    process.env.LUMOS_CONFIG_NAME !== 'AGGRON4' &&
-    existsSync(configFilePath)
-  ) {
-    process.env.LUMOS_CONFIG_FILE = configFilePath;
-  }
+  // if (
+  //   process.env.LUMOS_CONFIG_NAME !== 'LINA' &&
+  //   process.env.LUMOS_CONFIG_NAME !== 'AGGRON4' &&
+  //   existsSync(configFilePath)
+  // ) {
+  //   process.env.LUMOS_CONFIG_FILE = configFilePath;
+  // }
+  process.env.LUMOS_CONFIG_FILE = configFilePath;
   console.log(process.env.LUMOS_CONFIG_FILE);
   initializeConfig();
 }
