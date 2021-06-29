75cc37f6b4458b1fc81fc3b1968a9320c5d5d123
wangbing <wangbing@cryptape.com>
2021-06-29 10:46:28 +0800

    fix: test cli

--- offchain-modules/packages/app-cli/src/eth.ts
+++ offchain-modules/packages/app-cli/src/eth.ts
@@ -3,7 +3,7 @@ import { nonNullable } from '@force-bridge/x';
 import { Amount } from '@lay2/pw-core';
 import CKB from '@nervosnetwork/ckb-sdk-core';
 import commander from 'commander';
-import { ethers } from 'ethers';
+// import { ethers } from 'ethers';
 import { ckbPrivateKeyToAddress, parseOptions, waitUnlockCompleted } from './utils';
 
 const ForceBridgeRpc = 'http://127.0.0.1:8080/force-bridge/api/v1';
@@ -67,49 +67,56 @@ async function doLock(command: commander.Command, args: string[]) {
   const tokenSymbol = nonNullable(options.get('symbol') || 'ETH');
   const forceBridgeRpc = nonNullable(options.get('forceBridgeRpcUrl') || ForceBridgeRpc);
   const ethRpc = nonNullable(options.get('ethRpcUrl') || EthNodeRpc);
-
-  const assetList = await new ForceBridgeAPIV1Client(forceBridgeRpc).getAssetList();
-  const assetInfo = assetList.find((asset) => {
-    return asset.info.symbol === tokenSymbol;
-  });
-  if (assetInfo === undefined) {
-    console.log(`Invalid token symbol:${tokenSymbol}`);
-    return;
-  }
-
-  const mintPayload = {
-    sender: '0x0',
-    recipient: recipient,
-    asset: {
-      network: 'Ethereum',
-      ident: assetInfo.ident,
-      amount: new Amount(amount, assetInfo.info.decimals).toString(0),
-    },
-  };
-  const lockTx = nonNullable(
-    await new ForceBridgeAPIV1Client(forceBridgeRpc).generateBridgeInNervosTransaction(mintPayload),
-  );
-
-  const provider = new ethers.providers.JsonRpcProvider(ethRpc);
-  const wallet = new ethers.Wallet(privateKey, provider);
-
-  const unsignedTx = <ethers.PopulatedTransaction>lockTx.rawTransaction;
-  unsignedTx.nonce = await wallet.getTransactionCount();
-  unsignedTx.gasLimit = ethers.BigNumber.from(1000000);
-  unsignedTx.gasPrice = ethers.BigNumber.from(0);
-
-  const signedTx = await wallet.signTransaction(unsignedTx);
-  const lockRes = await provider.sendTransaction(signedTx);
-
-  console.log(
-    `Address:${wallet.address} locked:${amount} ${assetInfo.info.symbol}, recipient:${recipient}, lockTxHash:${lockRes.hash}`,
-  );
-
-  if (opts.wait) {
-    console.log('Waiting for transaction confirmed...');
-    await lockRes.wait(3);
-    console.log('Lock success.');
-  }
+  console.log(JSON.stringify(opts, undefined, 2));
+  console.log(JSON.stringify(privateKey, undefined, 2));
+  console.log(JSON.stringify(amount, undefined, 2));
+  console.log(JSON.stringify(recipient, undefined, 2));
+  console.log(JSON.stringify(tokenSymbol, undefined, 2));
+  console.log(JSON.stringify(forceBridgeRpc, undefined, 2));
+  console.log(JSON.stringify(ethRpc, undefined, 2));
+  //
+  // const assetList = await new ForceBridgeAPIV1Client(forceBridgeRpc).getAssetList();
+  // const assetInfo = assetList.find((asset) => {
+  //   return asset.info.symbol === tokenSymbol;
+  // });
+  // if (assetInfo === undefined) {
+  //   console.log(`Invalid token symbol:${tokenSymbol}`);
+  //   return;
+  // }
+  //
+  // const mintPayload = {
+  //   sender: '0x0',
+  //   recipient: recipient,
+  //   asset: {
+  //     network: 'Ethereum',
+  //     ident: assetInfo.ident,
+  //     amount: new Amount(amount, assetInfo.info.decimals).toString(0),
+  //   },
+  // };
+  // const lockTx = nonNullable(
+  //   await new ForceBridgeAPIV1Client(forceBridgeRpc).generateBridgeInNervosTransaction(mintPayload),
+  // );
+  //
+  // const provider = new ethers.providers.JsonRpcProvider(ethRpc);
+  // const wallet = new ethers.Wallet(privateKey, provider);
+  //
+  // const unsignedTx = <ethers.PopulatedTransaction>lockTx.rawTransaction;
+  // unsignedTx.nonce = await wallet.getTransactionCount();
+  // unsignedTx.gasLimit = ethers.BigNumber.from(1000000);
+  // unsignedTx.gasPrice = ethers.BigNumber.from(0);
+  //
+  // const signedTx = await wallet.signTransaction(unsignedTx);
+  // const lockRes = await provider.sendTransaction(signedTx);
+  //
+  // console.log(
+  //   `Address:${wallet.address} locked:${amount} ${assetInfo.info.symbol}, recipient:${recipient}, lockTxHash:${lockRes.hash}`,
+  // );
+  //
+  // if (opts.wait) {
+  //   console.log('Waiting for transaction confirmed...');
+  //   await lockRes.wait(3);
+  //   console.log('Lock success.');
+  // }
 }
 
 async function doUnlock(command: commander.Command, args: string[]) {
