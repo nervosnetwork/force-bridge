49526db13d691f58cf0826e25f2a00043725fae4
wangbing <wangbing@cryptape.com>
2021-06-30 00:28:19 +0800

    fix: fix keystore cli args parse bug

--- offchain-modules/packages/app-cli/src/keystore.ts
+++ offchain-modules/packages/app-cli/src/keystore.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import { KeyStore } from '@force-bridge/keystore';
+import { nonNullable } from '@force-bridge/x';
 import commander from 'commander';
 
 export const keystoreCmd = new commander.Command('keystore');
@@ -8,16 +9,10 @@ export const keystoreCmd = new commander.Command('keystore');
 keystoreCmd
   .command('encrypt')
   .description('encrypt a json file into a encrypted keystore')
-  .option('-p, --password [password]', 'password of the keystore')
-  .option('-s, --source [source]', 'path of a json file, the key is the name of a private key and value is the private key', './keys.json' )
-  .option('-d, --dist [dist]', 'path of the output file', './keystore.json')
+  .requiredOption('-p, --password <password>', 'password of the keystore')
+  .option('-s, --source <source>', 'path of a json file, the key is the name of a private key and value is the private key', './keys.json' )
+  .option('-d, --dist <dist>', 'path of the output file', './keystore.json')
   .action(convert);
-
-interface Options {
-  password: string;
-  source: string;
-  dist: string;
-}
 
 function asserts(condition: unknown, message: string): asserts condition {
   if (condition) return;
@@ -26,8 +21,10 @@ function asserts(condition: unknown, message: string): asserts condition {
   process.exit(1);
 }
 
-async function convert(command: commander.Command): Promise<void> {
-  const { password, source, dist } = command.opts() as Options;
+async function convert(opts: Record<string, string>): Promise<void> {
+  const password = nonNullable(opts.password);
+  const source = nonNullable(opts.source);
+  const dist = nonNullable(opts.dist);
 
   asserts(fs.existsSync(source), `${source} is not exists`);
   asserts(!!password, 'password is not valid');
