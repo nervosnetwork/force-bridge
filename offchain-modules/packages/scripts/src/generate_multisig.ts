0c92a4516f52f24985aa7c2c75b5a664ec6a2206
huwenchao <me@huwenchao.com>
2021-07-01 21:53:41 +0800

    use random generated privkeys

--- offchain-modules/packages/scripts/src/generate_multisig.ts
+++ offchain-modules/packages/scripts/src/generate_multisig.ts
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import path from 'path';
 import {
   getFromEnv,
   privateKeyToCkbPubkeyHash,
@@ -7,9 +7,21 @@ import {
 } from '@force-bridge/x/dist/utils';
 import * as lodash from 'lodash';
 
+const ETH_PRIVATE_KEY = '0xc4ad657963930fbff2e9de3404b30a4e21432c89952ed430b56bf802945ed37a';
+const CKB_PRIVATE_KEY = '0xa800c82df5461756ae99b5c6677d019c98cc98c7786b80d7b2e77256e46ea1fe';
+
+const genRanHex = (size) => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
+
 async function generateMultisig(multisigNumber: number, threshold: number) {
   const configPath = getFromEnv('CONFIG_PATH');
-  const privkeys = JSON.parse(fs.readFileSync(`${configPath}/privkeys.json`, 'utf8').toString());
+  const privkeys = {
+    eth: ETH_PRIVATE_KEY,
+    ckb: CKB_PRIVATE_KEY,
+  };
+  lodash.range(multisigNumber).map((i) => {
+    privkeys[`multisig-${i}`] = '0x' + genRanHex(64);
+  });
+  writeJsonToFile(privkeys, path.join(configPath, 'privkeys.json'));
   const config = {
     eth: {
       multiSignThreshold: threshold,
