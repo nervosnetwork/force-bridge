1aa2661521e1dddede350b3d5a471958e3f22a65
mkxbl <vyclzn@gmail.com>
2021-07-15 20:29:07 +0800

    chore: ci

--- offchain-modules/packages/x/src/db/entity/WithdrawedBridgeFee.ts
+++ offchain-modules/packages/x/src/db/entity/WithdrawedBridgeFee.ts
@@ -1,4 +1,4 @@
-import {Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn, Index} from 'typeorm';
+import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn, Index } from 'typeorm';
 
 @Entity()
 export class WithdrawedBridgeFee {
