f7d4a65c8c3d1f02a80372d1a96326d5c94ce0ed
wangbing <wangbing@cryptape.com>
2021-07-30 10:21:24 +0800

    fix: remove ckbPrivateKey & ethPrivateKey config item in monitor

--- offchain-modules/packages/scripts/src/testnetDocker.ts
+++ offchain-modules/packages/scripts/src/testnetDocker.ts
@@ -103,8 +103,6 @@ async function generateConfig(
   monitorConfig.common.role = 'watcher';
   monitorConfig.common.log.identity = 'monitor';
   monitorConfig.common.log.logFile = path.join(configPath, 'monitor/force_bridge.log');
-  monitorConfig.eth.privateKey = '';
-  monitorConfig.ckb.privateKey = '';
   monitorConfig.monitor = {
     discordWebHook: monitorDiscordWebHook,
     expiredTime: 1800000, //30 minutes
