ab942b01dbb66b8e074efaa2a3a24520f09bceb3
Wenchao Hu <zjuhwc@gmail.com>
2021-08-10 00:37:01 +0800

    fix: eth fork problem (#188)
    * fix: eth fork problem
    
    * fix ci

--- offchain-modules/packages/x/src/db/model.ts
+++ offchain-modules/packages/x/src/db/model.ts
@@ -65,6 +65,8 @@ export interface IEthLock {
   sudtExtraData?: string;
   blockNumber: number;
   blockHash: string;
+  confirmNumber?: number;
+  confirmStatus?: TxConfirmStatus;
 }
 
 export interface ICkbBurn {
