224d4d8021d8a0cd1d63b2af63a5ef892549d857
huwenchao <me@huwenchao.com>
2021-12-15 21:17:32 +0800

    doc: add docs and comments

--- eth-contracts/contracts/AssetManager.sol
+++ eth-contracts/contracts/AssetManager.sol
@@ -1,11 +1,19 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./interfaces/IMirrorToken.sol";
 
+/// @title Asset Manager - The entry to manage all bridged tokens from Nervos chain
+/// The owner of this contract should be transferred to the gnosis multi-sig wallet of Force Bridge committee when the
+/// initializing is done.
+/// All tokens recorded in this contract should be authorized to mint and burn by the this contract.
 contract AssetManager is Ownable, ReentrancyGuard {
+    // Mapping to keep track of the mirror tokens
+    // token is the address of the mirror token on Ethereum
+    // assetId is the identifier of the asset on Nervos Chain
     mapping(address => bytes32) public tokenToAssetIdMap;
     mapping(bytes32 => address) public assetIdToTokenMap;
 
@@ -14,12 +22,13 @@ contract AssetManager is Ownable, ReentrancyGuard {
     event Burn(bytes32 indexed assetId, address indexed token, address indexed from, uint256 amount);
 
     struct MintRecord {
-        bytes32 assetId;
-        address to;
-        uint256 amount;
-        bytes lockId;       // lockId on Nervos chain
+        bytes32 assetId;    // asset id locked on Nervos chain
+        address to;         // address of the receiver
+        uint256 amount;     // locked amount on Nervos chain
+        bytes lockId;       // used to identify the lock transaction on Nervos Chain
     }
 
+    /// @dev Add a new asset to the asset manager.
     function addAsset(address token, bytes32 assetId) public onlyOwner {
         require(tokenToAssetIdMap[token] == 0x0);
         require(assetIdToTokenMap[assetId] == address(0));
@@ -27,6 +36,7 @@ contract AssetManager is Ownable, ReentrancyGuard {
         assetIdToTokenMap[assetId] = token;
     }
 
+    /// @dev Remove an asset from the asset manager.
     function removeAsset(address token, bytes32 assetId) public onlyOwner {
         require(tokenToAssetIdMap[token] == assetId);
         require(assetIdToTokenMap[assetId] == token);
@@ -34,6 +44,9 @@ contract AssetManager is Ownable, ReentrancyGuard {
         assetIdToTokenMap[assetId] = address(0);
     }
 
+    /// @dev Mint an asset.
+    /// @param records The mint records.
+    /// This function should only be invoked by the gnosis multi-sig wallet of Force Bridge committee.
     function mint(MintRecord[] calldata records) public onlyOwner {
         for(uint i = 0; i < records.length; i++) {
             address tokenAddr = assetIdToTokenMap[records[i].assetId];
@@ -43,6 +56,9 @@ contract AssetManager is Ownable, ReentrancyGuard {
         }
     }
 
+    /// @dev Burn an asset and get the locked amount back on Nervos chain.
+    /// @param token The address of the token to burn.
+    /// @param amount The amount to burn.
     function burn(address token, uint256 amount) public nonReentrant {
         bytes32 assetId = tokenToAssetIdMap[token];
         require(assetId != 0x0, "token not supported");
--- eth-contracts/contracts/NervosMirrorToken.sol
+++ eth-contracts/contracts/NervosMirrorToken.sol
@@ -1,8 +1,10 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/// @title NervosMirrorToken - A token contract used for bridged token from Nervos Chain
 contract NervosMirrorToken is ERC20, Ownable {
     uint8 private _decimals;
 
