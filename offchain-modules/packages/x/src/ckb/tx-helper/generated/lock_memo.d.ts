12503e01b0fe348c5b83219f557f5f63a0918a6f
mkxbl <vyclzn@gmail.com>
2021-12-17 18:09:25 +0800

    chore: fmt

--- offchain-modules/packages/x/src/ckb/tx-helper/generated/lock_memo.d.ts
+++ offchain-modules/packages/x/src/ckb/tx-helper/generated/lock_memo.d.ts
@@ -14,19 +14,27 @@ export interface UnionType {
 }
 
 export function SerializeBytes(value: CanCastToArrayBuffer): ArrayBuffer;
+
 export class Bytes {
   constructor(reader: CanCastToArrayBuffer, options?: CreateOptions);
+
   validate(compatible?: boolean): void;
+
   indexAt(i: number): number;
+
   raw(): ArrayBuffer;
+
   length(): number;
 }
 
 export function SerializeLockMemo(value: object): ArrayBuffer;
+
 export class LockMemo {
   constructor(reader: CanCastToArrayBuffer, options?: CreateOptions);
+
   validate(compatible?: boolean): void;
+
   getXchain(): number;
+
   getRecipient(): Bytes;
 }
-
--- offchain-modules/packages/x/src/ckb/tx-helper/generated/unlock_memo.d.ts
+++ offchain-modules/packages/x/src/ckb/tx-helper/generated/unlock_memo.d.ts
@@ -14,27 +14,39 @@ export interface UnionType {
 }
 
 export function SerializeBytes(value: CanCastToArrayBuffer): ArrayBuffer;
+
 export class Bytes {
   constructor(reader: CanCastToArrayBuffer, options?: CreateOptions);
+
   validate(compatible?: boolean): void;
+
   indexAt(i: number): number;
+
   raw(): ArrayBuffer;
+
   length(): number;
 }
 
 export function SerializeBytesVec(value: Array<CanCastToArrayBuffer>): ArrayBuffer;
+
 export class BytesVec {
   constructor(reader: CanCastToArrayBuffer, options?: CreateOptions);
+
   validate(compatible?: boolean): void;
+
   indexAt(i: number): Bytes;
+
   length(): number;
 }
 
 export function SerializeUnlockMemo(value: object): ArrayBuffer;
+
 export class UnlockMemo {
   constructor(reader: CanCastToArrayBuffer, options?: CreateOptions);
+
   validate(compatible?: boolean): void;
+
   getXchain(): number;
+
   getBurnIds(): BytesVec;
 }
-
