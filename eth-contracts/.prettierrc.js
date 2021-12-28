b743e3f669ba519f48cc878dc9fdf1d04ac2a043
huwenchao <me@huwenchao.com>
2021-12-28 21:43:54 +0800

    change prettier config

--- eth-contracts/.prettierrc.js
+++ eth-contracts/.prettierrc.js
@@ -1,5 +1,22 @@
+// module.exports = {
+//   tabWidth: 2,
+//   semi: true,
+//   singleQuote: true,
+// };
+//
 module.exports = {
   tabWidth: 2,
-  semi: true,
   singleQuote: true,
+  semi: true,
+  overrides: [
+    {
+      files: '*.sol',
+      options: {
+        tabWidth: 2,
+        singleQuote: true,
+        explicitTypes: 'always',
+      },
+    },
+  ],
+  plugins: [require.resolve('prettier-plugin-solidity')],
 };
