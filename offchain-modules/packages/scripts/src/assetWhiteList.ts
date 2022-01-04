50c7bd78c332a9b8d6e0130b1c1d15b85672e740
huwenchao <me@huwenchao.com>
2022-01-05 00:37:34 +0800

    feat: add bsc asset white list

--- offchain-modules/packages/scripts/src/assetWhiteList.ts
+++ offchain-modules/packages/scripts/src/assetWhiteList.ts
@@ -5,6 +5,7 @@ import { WhiteListEthAsset } from '@force-bridge/x/dist/config';
 import { writeJsonToFile } from '@force-bridge/x/dist/utils';
 import { getAssetAVGPrice, getClosestNumber } from '@force-bridge/x/dist/utils/price';
 import { BigNumber } from 'bignumber.js';
+import { ethers } from 'ethers';
 
 export interface Token {
   address: string;
@@ -18,8 +19,14 @@ export interface WhiteListEthAssetExtend extends WhiteListEthAsset {
   sudtArgs: string;
 }
 
+export type Network = 'Ethereum' | 'BSC';
+
 const BRIDGE_IN_CKB_FEE = 400; // 400CKB
 const BRIDGE_OUT_ETH_FEE = 0.015; // 15W gas * 100Gwei
+const TOKEN_PRICE_MAPPING = {
+  BTCB: 'BTC',
+  WBNB: 'BNB',
+};
 
 async function getBridgeInFeeInUSDT(): Promise<BigNumber> {
   const price = await getAssetAVGPrice('CKB');
@@ -32,15 +39,27 @@ async function getBridgeOutFeeInUSDT(): Promise<BigNumber> {
 }
 
 //  eslint-disable-next-line @typescript-eslint/no-unused-vars
-async function generateWhiteList(inPath: string, outPath: string): Promise<void> {
+async function generateWhiteList(inPath: string, outPath: string, network: Network): Promise<void> {
   const tokens: Token[] = JSON.parse(fs.readFileSync(inPath, 'utf8'));
   const assetWhiteList: WhiteListEthAsset[] = [];
   let price = new BigNumber(1);
   const bridgeInFee = await getBridgeInFeeInUSDT();
-  const bridgeOutFee = await getBridgeOutFeeInUSDT();
+  let bridgeOutFee;
+  switch (network) {
+    case 'Ethereum':
+      bridgeOutFee = await getBridgeOutFeeInUSDT();
+      break;
+    case 'BSC':
+      bridgeOutFee = bridgeInFee;
+      break;
+    default:
+      throw new Error(`unknown network: ${network}`);
+  }
   for (const token of tokens) {
     if (['USDT', 'DAI'].includes(token.symbol)) {
       price = new BigNumber(1);
+    } else if (TOKEN_PRICE_MAPPING[token.symbol] !== undefined) {
+      price = new BigNumber(await getAssetAVGPrice(TOKEN_PRICE_MAPPING[token.symbol]));
     } else {
       price = new BigNumber(await getAssetAVGPrice(token.symbol));
     }
@@ -48,12 +67,13 @@ async function generateWhiteList(inPath: string, outPath: string): Promise<void>
     const minimalBridgeAmount = baseAmount.times(2).multipliedBy(bridgeOutFee).toFixed(0);
     const inAmount = baseAmount.multipliedBy(new BigNumber(bridgeInFee)).toFixed(0);
     const outAmount = baseAmount.multipliedBy(new BigNumber(bridgeOutFee)).toFixed(0);
+    token.address = ethers.utils.getAddress(token.address);
 
     const assetInfo: WhiteListEthAssetExtend = {
       ...token,
       minimalBridgeAmount: getClosestNumber(minimalBridgeAmount),
       bridgeFee: { in: getClosestNumber(inAmount), out: getClosestNumber(outAmount) },
-      sudtArgs: addressToSudtArgs(token.address),
+      sudtArgs: addressToSudtArgs(token.address, network),
     };
     assetWhiteList.push(assetInfo);
     console.log(`info: ${JSON.stringify(assetInfo)}, price: ${price}`);
@@ -61,12 +81,23 @@ async function generateWhiteList(inPath: string, outPath: string): Promise<void>
   writeJsonToFile(assetWhiteList, outPath);
 }
 
-function addressToSudtArgs(address: string): string {
+function addressToSudtArgs(address: string, network: Network): string {
   // mainnet config
+  let args = '0x';
+  switch (network) {
+    case 'Ethereum':
+      args = '0x36a3a692465d2fd3e855078280cba526d90c8b5c98c5da1c1f4430e1086ca602';
+      break;
+    case 'BSC':
+      args = '0x07bfdc1b96a5b5d3d91b7406d5583f3921e58ce6ba54b6dfd299bd6c7031cbff';
+      break;
+    default:
+      throw new Error(`unknown network: ${network}`);
+  }
   const ownerCellTypeHash = utils.computeScriptHash({
     code_hash: '0x00000000000000000000000000000000000000000000000000545950455f4944',
     hash_type: 'type',
-    args: '0x36a3a692465d2fd3e855078280cba526d90c8b5c98c5da1c1f4430e1086ca602',
+    args,
   });
   const bridgeLockscript = {
     code_hash: '0x93bc7a915d3d8f8b9678bc6c7a1751738c99ce6e66bba4dfab56672f6d691789',
@@ -78,7 +109,16 @@ function addressToSudtArgs(address: string): string {
 }
 
 async function main() {
-  await generateWhiteList('../configs/raw-mainnet-asset-white-list.json', '../configs/mainnet-asset-white-list.json');
+  await generateWhiteList(
+    '../configs/raw-mainnet-asset-white-list.json',
+    '../configs/mainnet-asset-white-list.json',
+    'Ethereum',
+  );
+  await generateWhiteList(
+    '../configs/bsc-raw-mainnet-asset-white-list.json',
+    '../configs/bsc-mainnet-asset-white-list.json',
+    'BSC',
+  );
 }
 
 main()
