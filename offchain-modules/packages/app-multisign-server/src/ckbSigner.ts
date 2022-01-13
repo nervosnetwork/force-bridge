ff719c8da3163a0e5f7c79c3ec247f8379117b30
pygman <pygman@aliyun.com>
2022-01-14 00:57:06 +0800

    feat: collect unlock signatures from verifiers

--- offchain-modules/packages/app-multisign-server/src/ckbSigner.ts
+++ offchain-modules/packages/app-multisign-server/src/ckbSigner.ts
@@ -1,4 +1,4 @@
-import { Cell } from '@ckb-lumos/base';
+import { Cell, utils } from '@ckb-lumos/base';
 import { common } from '@ckb-lumos/common-scripts';
 import { key } from '@ckb-lumos/hd';
 import {
@@ -9,7 +9,7 @@ import {
   TransactionSkeletonObject,
 } from '@ckb-lumos/helpers';
 import { BtcAsset, ChainType, EosAsset, EthAsset, TronAsset } from '@force-bridge/x/dist/ckb/model/asset';
-import { getOwnerTypeHash } from '@force-bridge/x/dist/ckb/tx-helper/multisig/multisig_helper';
+import { getFromAddr, getOwnerTypeHash } from '@force-bridge/x/dist/ckb/tx-helper/multisig/multisig_helper';
 import { ForceBridgeCore } from '@force-bridge/x/dist/core';
 import { EthLock } from '@force-bridge/x/dist/db/entity/EthLock';
 import { asserts, nonNullable } from '@force-bridge/x/dist/errors';
@@ -20,12 +20,16 @@ import {
   ckbUnlockCollectSignaturesPayload,
   collectSignaturesParams,
   mintRecord,
+  unlockRecord,
 } from '@force-bridge/x/dist/multisig/multisig-mgr';
 import { verifyCollector } from '@force-bridge/x/dist/multisig/utils';
 import { compareCkbAddress } from '@force-bridge/x/dist/utils';
 import { Amount } from '@lay2/pw-core';
 import { SigError, SigErrorCode, SigErrorOk } from './error';
 import { SigResponse, SigServer } from './sigServer';
+import { getOmniLockMultisigAddress } from '@force-bridge/x/dist/ckb/tx-helper/multisig/omni-lock';
+import { EthBurn } from '@force-bridge/x/dist/db/entity/EthBurn';
+import { CkbUnlock } from '@force-bridge/x/dist/db/entity/CkbUnlock';
 
 async function verifyCreateCellTx(rawData: string, payload: ckbCreateCellCollectSignaturesPayload): Promise<SigError> {
   const txSkeleton = payload.txSkeleton;
@@ -116,6 +120,23 @@ async function verifyDuplicateMintTx(
   return SigErrorOk;
 }
 
+async function verifyDuplicateUnlockTx(
+  pubKey: string,
+  unlockRecords: unlockRecord[],
+  _txSkeleton: TransactionSkeletonObject,
+): Promise<SigError> {
+  const refTxHashes = unlockRecords.map((mintRecord) => {
+    return mintRecord.id;
+  });
+
+  const unlocks = await SigServer.ckbDb.getCkbUnlockByIds(refTxHashes);
+  if (unlocks.length !== 0) {
+    return new SigError(SigErrorCode.TxCompleted);
+  }
+
+  return SigErrorOk;
+}
+
 async function verifyMintTx(
   pubKey: string,
   rawData: string,
@@ -185,13 +206,73 @@ async function verifyMintTx(
 }
 
 async function verifyUnlockTx(
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   pubKey: string,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   rawData: string,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   payload: ckbUnlockCollectSignaturesPayload,
 ): Promise<SigError> {
+  const collectorScript = parseAddress(getFromAddr());
+  const omniLockMultisigScript = parseAddress(getOmniLockMultisigAddress());
+  const txSkeleton = payload.txSkeleton;
+  const sigData = txSkeleton.signingEntries[1].message;
+  if (rawData !== sigData) {
+    return new SigError(SigErrorCode.InvalidParams, `rawData:${rawData} doesn't match with:${sigData}`);
+  }
+
+  const unlockRecords = payload.unlockRecords;
+  asserts(unlockRecords);
+
+  let err = await verifyDuplicateUnlockTx(pubKey, unlockRecords, txSkeleton);
+  if (err.Code !== SigErrorCode.Ok) {
+    return err;
+  }
+
+  const sudtTypescript = ForceBridgeCore.config.ckb.deps.sudtType.script;
+  if (
+    txSkeleton.outputs.some(
+      (output) => output.cell_output.type && output.cell_output.type.code_hash !== sudtTypescript.codeHash,
+    )
+  ) {
+    return new SigError(SigErrorCode.InvalidParams, `unlock recode outputs contains non-sudt typescript`);
+  }
+  const unlockCells = txSkeleton.outputs.filter(
+    (cell) =>
+      cell.cell_output.lock.code_hash !== collectorScript.code_hash &&
+      cell.cell_output.lock.code_hash !== omniLockMultisigScript.code_hash,
+  );
+  if (unlockRecords.length !== unlockCells.length) {
+    return new SigError(
+      SigErrorCode.InvalidParams,
+      `unlok recode length:${unlockRecords.length} doesn't match with:${unlockCells.length}`,
+    );
+  }
+
+  const typeArgs = unlockCells.map((output) => {
+    const typescript = output.cell_output.type;
+    return typescript ? typescript.args : '';
+  });
+
+  if (typeArgs.length > 1) {
+    return new SigError(SigErrorCode.InvalidParams, `unlock recode outputs contains not single typescript`);
+  }
+
+  for (let i = 0; i < unlockRecords.length; i++) {
+    const unlockRecord = unlockRecords[i];
+    if (unlockRecord.xchain !== ChainType.ETH) {
+      //those chains doesn't verify now
+      continue;
+    }
+
+    const output = unlockCells[i];
+    err = await verifyEthForUnlockTx(unlockRecord, output);
+    if (err.Code !== SigErrorCode.Ok) {
+      return err;
+    }
+  }
+  err = await verifyEthForUnlockRecords(unlockRecords);
+  if (err.Code !== SigErrorCode.Ok) {
+    return err;
+  }
+
   return SigErrorOk;
 }
 
@@ -229,6 +310,63 @@ async function verifyEthMintRecords(records: mintRecord[]): Promise<SigError> {
       return new SigError(
         SigErrorCode.InvalidRecord,
         `invalid mint amount ${record.amount}, greater than lock amount ${ethLock.amount}`,
+      );
+    }
+  }
+  return SigErrorOk;
+}
+
+async function verifyEthForUnlockRecords(records: unlockRecord[]): Promise<SigError> {
+  const unlockIds = records.map((record) => {
+    return record.id;
+  });
+  const ckbUnlocks = await SigServer.ckbDb.getCkbUnlockByIds(unlockIds);
+  const ckbUnlockMap: { [k: string]: CkbUnlock } = Object.fromEntries(
+    ckbUnlocks.map((ckbUnlock) => [ckbUnlock.id, ckbUnlock]),
+  );
+  const burnTxHashes = ckbUnlocks.map((ckbUnlock) => ckbUnlock.burnTxHash);
+  const ethBurns = await SigServer.ethDb.getEthBurnsByBurnTxHashes(burnTxHashes);
+  const ethBurnMap: { [k: string]: EthBurn } = Object.fromEntries(
+    ethBurns.map((ethBurn) => [ethBurn.burnTxHash, ethBurn]),
+  );
+
+  for (const record of records) {
+    const ckbUnlock = ckbUnlockMap[record.id];
+    if (!ckbUnlock) {
+      return new SigError(SigErrorCode.TxNotFound, `cannot found ckb unlock tx by id:${record.id}`);
+    }
+    if (ckbUnlock.burnTxHash !== record.burnTxHash) {
+      return new SigError(
+        SigErrorCode.TxUnconfirmed,
+        `ethereumForUnlockTx unequal burnTxHash id: ${record.id}, ${ckbUnlock.burnTxHash} !== ${record.burnTxHash}`,
+      );
+    }
+    const ethBurn = ethBurnMap[ckbUnlock.burnTxHash];
+    if (!ethBurn) {
+      return new SigError(
+        SigErrorCode.TxNotFound,
+        `cannot found ethereum burn tx by txHash: ${ckbUnlock.burnTxHash}, ckb unlock id: ${record.id}`,
+      );
+    }
+    if (ethBurn.confirmStatus !== 'confirmed') {
+      return new SigError(SigErrorCode.TxUnconfirmed, `ethereumForUnlockTx:${ethBurn.burnTxHash} doesn't confirmed`);
+    }
+    if (record.recipientAddress !== ethBurn.recipient) {
+      return new SigError(
+        SigErrorCode.InvalidRecord,
+        `recipientAddress: ${record.recipientAddress} !== recipient: ${ethBurn.recipient}`,
+      );
+    }
+    if (record.assetIdent !== ethBurn.nervosAssetId) {
+      return new SigError(
+        SigErrorCode.InvalidRecord,
+        `ethereumForUnlockTxHash:${ethBurn.burnTxHash} assetIdent:${record.assetIdent} != ${ethBurn.nervosAssetId}`,
+      );
+    }
+    if (BigInt(record.amount) > BigInt(ethBurn.amount)) {
+      return new SigError(
+        SigErrorCode.InvalidRecord,
+        `invalid unlock amount ${record.amount}, greater than lock amount ${ethBurn.amount}`,
       );
     }
   }
@@ -287,6 +425,58 @@ async function verifyEthMintTx(mintRecord: mintRecord, output: Cell): Promise<Si
   const data = amount.toUInt128LE();
   if (data !== output.data) {
     return new SigError(SigErrorCode.InvalidRecord, `data:${output.data} doesn't with ${data}`);
+  }
+  return SigErrorOk;
+}
+
+async function verifyEthForUnlockTx(unlockRecord: unlockRecord, output: Cell): Promise<SigError> {
+  const lockScript = output.cell_output.lock;
+  const recipientLockscript = parseAddress(unlockRecord.recipientAddress);
+  if (lockScript.code_hash !== recipientLockscript.code_hash) {
+    return new SigError(
+      SigErrorCode.InvalidRecord,
+      `lockScript code_hash:${lockScript.code_hash} doesn't match with:${recipientLockscript.code_hash}`,
+    );
+  }
+  if (lockScript.hash_type !== recipientLockscript.hash_type) {
+    return new SigError(
+      SigErrorCode.InvalidRecord,
+      `lockScript hash_type:${lockScript.hash_type} doesn't match with:${recipientLockscript.hash_type}`,
+    );
+  }
+  if (lockScript.args !== recipientLockscript.args) {
+    return new SigError(
+      SigErrorCode.InvalidRecord,
+      `lockScript args:${lockScript.args} doesn't match with:${recipientLockscript.args}`,
+    );
+  }
+
+  const typeScript = output.cell_output.type;
+  const sudtTypescript = ForceBridgeCore.config.ckb.deps.sudtType.script;
+  if (typeScript) {
+    if (typeScript.code_hash !== sudtTypescript.codeHash) {
+      return new SigError(
+        SigErrorCode.InvalidRecord,
+        `typescript code_hash:${typeScript.code_hash} doesn't match with:${sudtTypescript.codeHash}`,
+      );
+    }
+    if (typeScript.hash_type !== sudtTypescript.hashType) {
+      return new SigError(
+        SigErrorCode.InvalidRecord,
+        `typescript hash_type:${typeScript.hash_type} doesn't match with:${sudtTypescript.hashType}`,
+      );
+    }
+    const data = utils.toBigUInt128LE(BigInt(unlockRecord.amount));
+    if (data !== output.data) {
+      return new SigError(SigErrorCode.InvalidRecord, `data:${output.data} doesn't with ${data}`);
+    }
+  } else {
+    if (BigInt(unlockRecord.amount) !== BigInt(output.cell_output.capacity)) {
+      return new SigError(
+        SigErrorCode.InvalidRecord,
+        `capacity: ${unlockRecord.amount} doesn't with ${output.cell_output.capacity}`,
+      );
+    }
   }
   return SigErrorOk;
 }
